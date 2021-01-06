3e42dcaea4b6558a337d4ec5e87c1a30f2b2ceba
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2021-01-06 14:16:14 -0500

    Market Place Contract

---
+++ contracts/KittyMarketPlace.sol
@@ -0,0 +1,105 @@
+pragma solidity ^0.5.12;
+
+import "./CatContract.sol";
+import "./Ownable.sol";
+import "./IKittyMarketPlace.sol";
+
+contract KittyMarketPlace is Ownable, IKittyMarketPlace {
+  CatContract private _CatContract;
+
+  struct Offer{
+    address payable seller;
+    uint256 price;
+    uint256 index;
+    uint256 tokenID;
+    bool active;
+  }
+
+  Offer[] offers;
+
+  mapping(uint256 => Offer) tokenIDToOffer;
+
+  event MarketTransaction(string TxType, address owner, uint256 tokenID);
+
+  constructor(address _CatContractAddress) public {
+    setKittyContract(_CatContractAddress);
+  }
+
+  function setKittyContract(address _CatContractAddress) public onlyOwner{
+    _CatContract = CatContract(_CatContractAddress);
+  }
+
+  function getOffer(uint256 _tokenID) public view returns (address seller, uint256 price, uint256 index, uint256 tokenID, bool active){
+    Offer storage offer = tokenIDToOffer[_tokenID];
+
+    return (offer.seller, offer.price, offer.index, offer.tokenID, offer.active);
+  }
+
+  function getAllTokenOnSale() public view returns(uint256[] memory listOfOffers){
+    uint256[] memory forSaleCats = new uint256[](offers.length);
+
+    if(offers.length == 0){
+      return new uint256[](0);
+    } else{
+    for(uint256 i = 0; i < offers.length; i++){
+      if(offers[i].active == true){
+        forSaleCats[i] = offers[i].tokenID;
+      }
+    }
+    return forSaleCats;
+    }
+  }
+
+  function setOffer(uint256 _price, uint256 _tokenID) public{
+    require(_owns(msg.sender, _tokenID), "You must own the cat");
+    require(tokenIDToOffer[_tokenID].active == false, "Cannot double sell the cat");
+    require(_CatContract.isApprovedForAll(msg.sender, address(this)), "Contract must be approved operator for offer to be created");
+    require(_price > 0, "No free cats here buddy");
+
+    Offer memory offer = Offer(
+      address(msg.sender),
+      uint256(_price),
+      uint256(offers.length),
+      uint256(_tokenID),
+      bool(true)
+    );
+
+    tokenIDToOffer[_tokenID] = offer;
+
+    offers.push(offer);
+
+    emit MarketTransaction("Offer Created", msg.sender, _tokenID);
+  }
+
+  function _owns(address _claimant, uint256 _tokenID) internal view returns(bool){
+    return _CatContract.ownerOf(_tokenID) == _claimant;
+  }
+
+  function removeOffer(uint256 _tokenID) public{
+    Offer memory offer = tokenIDToOffer[_tokenID];
+
+    require(offer.seller == msg.sender, "Must be the seller/owner to remove an offer");
+
+    delete offers[offer.index];
+    tokenIDToOffer[_tokenID].active = false;
+
+    emit MarketTransaction("Offer Removed", msg.sender, _tokenID);
+  }
+
+
+  function buyKitty(uint256 _tokenID) public payable{
+    Offer memory offer = tokenIDToOffer[_tokenID];
+
+    require(offer.price == msg.value, "Payment must be equal to price of cat");
+    require(offer.active == true, "Offer must be active");
+
+    delete offers[offer.index];
+    tokenIDToOffer[_tokenID].active = false;
+
+    offer.seller.transfer(offer.price);
+
+    _CatContract.transferFrom(tokenIDToOffer[_tokenID].seller, msg.sender, _tokenID);
+
+    emit MarketTransaction("Cat Bought", msg.sender, _tokenID);
+  }
+}
