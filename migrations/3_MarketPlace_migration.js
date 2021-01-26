ec486aa0fb823bdd2037515979859859c21e7ea4
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2021-01-26 11:03:14 -0500

    Delete 3_MarketPlace_migration.js

--- migrations/3_MarketPlace_migration.js
+++
@@ -1,6 +0,0 @@
-const CatContract = artifacts.require("CatContract");
-const KittyMarketPlace = artifacts.require("KittyMarketPlace");
-
-module.exports = function(deployer) {
-  deployer.deploy(KittyMarketPlace, CatContract.address);
-};
