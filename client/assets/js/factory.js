493b63079c17bc273a391186082026033470059e
Ty-Sir <67834313+Ty-Sir@users.noreply.github.com>
2021-02-25 17:49:08 -0500

    JS for Factory

--- client/assets/js/factory.js
+++ client/assets/js/factory.js
@@ -2,12 +2,13 @@ let web3 = new Web3(Web3.givenProvider);
 
 let catContractInstance;
 let user;
-let catContractAddress = "0x3e4DCa599A892E726125CcC6b241dCff0b019fa2";//enter the catcontract address after you migrate
+let catContractAddress = "";//enter contract here after migration
 
 $(document).ready(function(){
   window.ethereum.enable().then(function(accounts){
     catContractInstance = new web3.eth.Contract(abi.CatContract, catContractAddress, {from: accounts[0]});
     user = accounts[0];
+    openingModals();
 
     console.log(catContractInstance);
 
@@ -28,8 +29,36 @@ $(document).ready(function(){
                             '<big>' + " Kitten ID: " + '</big>' + tokenID +
                             '<big>' + " Genes: " + '</big>' + genes);
       closeIcon();
-    }).on('error', console.error);
+    }).on('error', (error, receipt) => console.log(error, receipt));
   })
+
+});
+
+async function openingModals(){
+  try{
+    let gen0Created = await catContractInstance.methods.gen0Counter().call();
+    console.log(gen0Created);
+
+    if(gen0Created < 1){
+      $('#howManyCatsLeftToMake').html("So far " + gen0Created + " cats have been made.")
+    }else if(gen0Created == 1){
+      $('#howManyCatsLeftToMake').html("So far " + gen0Created + " cat has been made.")
+    }else{
+      $('#howManyCatsLeftToMake').html("So far " + gen0Created + " cats have been made.")
+    };
+
+    if(gen0Created < 30){
+      $('#openingModal').modal('show');
+    } else{
+      $('#ifAllCatsMade').modal('show');
+    }
+  } catch(err){
+    console.log(err);
+  }
+};
+
+$('#continueBtn').click(function(){
+  $('#openingModal').modal('hide');
 });
 
 //close-icon button for created cat alert
